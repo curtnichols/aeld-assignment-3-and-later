87fae1d3aa7d758d680cb883f272733e60580f10
Curt Nichols <curt.nichols@live.com>
2026-09-15 15:58:22 -0500

    Checkpoint: Assignment 4, Part 1

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -14,6 +14,9 @@ struct thread_data{
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
      */
+    pthread_mutex_t *mutex;
+    int wait_to_obtain_ms;
+    int wait_to_release_ms;
 
     /**
      * Set to true if the thread completed with success, false
@@ -37,4 +40,8 @@ struct thread_data{
 * coresponding to the thread which was started.
 * @return true if the thread could be started, false if a failure occurred.
 */
-bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms);
+bool start_thread_obtaining_mutex(
+    pthread_t *thread, 
+    pthread_mutex_t *mutex,
+    int wait_to_obtain_ms, 
+    int wait_to_release_ms);
