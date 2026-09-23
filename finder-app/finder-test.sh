856469385dc57c719468adb4e7b21a4a06359ff3
Curt Nichols <curt.nichols@live.com>
2026-09-23 15:27:03 -0500

    Strip relative path from ./finder.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -65,7 +65,7 @@ do
 	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
